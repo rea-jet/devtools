751af2e3733da3dd75cd75f03965aea0889f2ceb
Dennis <github.com@mail.ds82.de>
2018-11-07 11:38:07 +0100

    feat(jest): add testMatch & transform config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx']
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  testMatch: [
+    '**/__tests__/**/*.(js|ts)?(x)',
+    '**/?(*.)+(spec|test).(js|ts)?(x)'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  }
 };
