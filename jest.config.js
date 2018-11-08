3b2fa3293eeb337b089f4bcd160070db6b7b06a5
Dennis <github.com@mail.ds82.de>
2018-11-08 13:09:52 +0100

    feat(jest): ignore build directories

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,11 @@ module.exports = {
     '**/__tests__/**/*.(js|ts)?(x)',
     '**/?(*.)+(spec|test).(js|ts)?(x)'
   ],
+  testPathIgnorePatterns: [
+    '<rootDir>/build/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/'
+  ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
   }
