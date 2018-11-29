3b48d4a349c54a3e4b0cc12dfc6bd1f32104218d
Dennis <github.com@mail.ds82.de>
2018-11-29 10:23:04 +0100

    fix(babel): decorator before class prop plugin
    see:
    https://github.com/loganfsmyth/babel-plugin-transform-decorators-legacy#note-order-of-plugins-matters

--- babelrc.js
+++ babelrc.js
@@ -19,11 +19,11 @@ module.exports = {
     }
   },
   plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     '@babel/plugin-transform-modules-commonjs',
     '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
     '@babel/plugin-transform-react-constant-elements',
     '@babel/plugin-proposal-function-bind',
     [
