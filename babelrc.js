ad9a5f4290e56906107e67074c47451bbefd5352
Dennis <github.com@mail.ds82.de>
2018-12-14 13:36:41 +0100

    feat(babel): enable class properties loose mode
    see https://babeljs.io/docs/en/next/babel-plugin-proposal-decorators.html

--- babelrc.js
+++ babelrc.js
@@ -22,7 +22,7 @@ module.exports = {
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     '@babel/plugin-transform-modules-commonjs',
     '@babel/plugin-transform-runtime',
-    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-react-constant-elements',
     '@babel/plugin-proposal-function-bind',
