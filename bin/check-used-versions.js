b3bfa634184924f7b9cffd1cf3fd2ff61bc58299
Daniel Schuech <dschuech@rea.de>
2019-07-29 12:59:56 +0200

    refactor(*): more utils from check-used-versions to helper

--- bin/check-used-versions.js
+++ bin/check-used-versions.js
@@ -2,56 +2,16 @@
 const argv = require('yargs').argv;
 const fs = require('fs');
 const pathN = require('path');
-const {
-  nth,
-  flatten,
-  pipe,
-  map,
-  filter,
-  curry,
-  path,
-  isNil,
-  complement,
-  reduce,
-  set,
-  lensPath,
-  over
-} = require('ramda');
+const { pipe, map, filter, path, reduce } = require('ramda');
 
 const {
-  getRootDir,
   getPackageDirs,
-  expandDirWhenNeeded,
-  expandDir,
-  addPackageJson
+  addPackageJson,
+  extractVersionOf,
+  buildDatabase
 } = require('./helper');
 
 const package = argv.name;
-const rootDir = getRootDir();
-
-const extractVersionOf = curry((package, pkgUri) => {
-  const pkg = require(pathN.join(rootDir, pkgUri));
-
-  return [
-    path(['name'], pkg),
-    path(['dependencies', package], pkg),
-    path(['devDependencies', package], pkg)
-  ];
-});
-
-const addEntry = curry((name, list) => [...(list || []), name]);
-
-const setVersion = curry((prodOrDev, pkgName, version, db) => {
-  return version
-    ? over(lensPath([version, prodOrDev]), addEntry(pkgName), db)
-    : db;
-});
-
-const buildDatabase = (acc, [pkgName, prodVersion, devVersion]) =>
-  pipe(
-    setVersion('prod', pkgName, prodVersion),
-    setVersion('dev', pkgName, devVersion)
-  )(acc);
 
 const printResult = db => {
   const numberOfVersions = Object.keys(db).length;
--- bin/helper.js
+++ bin/helper.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
 const pathN = require('path');
-const { map, flatten, pipe, reduce } = require('ramda');
+const {
+  map,
+  flatten,
+  pipe,
+  reduce,
+  curry,
+  path,
+  over,
+  lensPath
+} = require('ramda');
 
 const getRootDir = () => pathN.resolve(process.cwd());
+
+const addEntry = curry((name, list) => [...(list || []), name]);
 
 const packageDirsFromLerna = () => {
   return [
@@ -13,24 +24,46 @@ const packageDirsFromLerna = () => {
 
 const expandDir = dir =>
   fs
-    .readdirSync(dir)
-    .map(file => [file, fs.statSync(pathN.join(dir, file))])
+    .readdirSync(pathN.resolve(getRootDir(), dir))
+    .map(file => [file, fs.statSync(pathN.join(getRootDir(), dir, file))])
     .filter(([file, stats]) => stats.isDirectory())
     .map(([file, _]) => pathN.join(dir, file));
 
-const expandDirWhenNeeded = dir =>
-  !!dir.match(/\*$/) ? expandDir(pathN.dirname(dir)) : dir;
-
+const expandDirWhenNeeded = dir => {
+  return !!dir.match(/\*$/) ? expandDir(pathN.dirname(dir)) : dir;
+};
 const getPackageDirs = () =>
   pipe(
     map(expandDirWhenNeeded),
     flatten
   )(packageDirsFromLerna());
 
-const addPackageJson = uri => pathN.join(uri, 'package.json');
+const addPackageJson = uri => pathN.join(getRootDir(), uri, 'package.json');
 
 const pipeM = (...[fst, ...pipeline]) => (...args) =>
   reduce((acc, fn) => acc.then(fn), Promise.resolve(fst(...args)), pipeline);
+
+const extractVersionOf = curry((package, pkgUri) => {
+  const pkg = require(pathN.join(pkgUri));
+
+  return [
+    path(['name'], pkg),
+    path(['dependencies', package], pkg),
+    path(['devDependencies', package], pkg)
+  ];
+});
+
+const setVersion = curry((prodOrDev, pkgName, version, db) => {
+  return version
+    ? over(lensPath([version, prodOrDev]), addEntry(pkgName), db)
+    : db;
+});
+
+const buildDatabase = (acc, [pkgName, prodVersion, devVersion]) =>
+  pipe(
+    setVersion('prod', pkgName, prodVersion),
+    setVersion('dev', pkgName, devVersion)
+  )(acc);
 
 module.exports = {
   getRootDir,
@@ -38,5 +71,7 @@ module.exports = {
   expandDir,
   expandDirWhenNeeded,
   addPackageJson,
-  pipeM
+  pipeM,
+  extractVersionOf,
+  buildDatabase
 };
