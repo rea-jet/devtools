faa0747ec40d76c3b1723de622b01a6f6da23b05
Daniel Schuech <dschuech@rea.de>
2019-08-26 09:44:34 +0200

    feat(bin): check hoisting for multiple packages

--- bin/check-hoisting.js
+++ bin/check-hoisting.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-const argv = require('yargs').argv;
+const yargs = require('yargs');
 const fs = require('fs');
 const pathN = require('path');
-const { pipe, map, filter, path, reduce } = require('ramda');
+const { pipe, map, filter, path, reduce, and } = require('ramda');
 
 const {
   getPackageDirs,
@@ -12,14 +12,14 @@ const {
   getRootDir
 } = require('./helper');
 
-const package = argv.name;
+const packages = yargs.array('name').parse().name;
 
-const checkVersionResults = db => {
+const checkVersionResults = package => db => {
   const numberOfVersions = Object.keys(db).length;
 
   if (numberOfVersions === 0) {
     console.log(`No Version for ${package} found in packages.`);
-    process.exit(1);
+    return false;
   }
 
   if (numberOfVersions > 1) {
@@ -27,31 +27,49 @@ const checkVersionResults = db => {
       `There are ${numberOfVersions} different versions of ${package}:`
     );
     console.log(JSON.stringify(db, null, 2));
-    process.exit(1);
+    return false;
   }
 
   return Object.keys(db)[0];
 };
 
-const checkInstalledVersion = version => {
+const checkInstalledVersion = package => version => {
+  if (!version) {
+    return false;
+  }
+
   const packageVersion = require(`${getRootDir()}/node_modules/${package}/package.json`)
     .version;
   if (packageVersion !== version) {
     console.log(
       `Required version of ${package} is ${version} but found ${packageVersion} installed`
     );
-    process.exit(1);
+    return false;
   } else {
     console.log(`All good for ${package}`);
-    process.exit(0);
+    return true;
   }
 };
 
-pipe(
+const packageTree = pipe(
   map(addPackageJson),
-  filter(fs.existsSync.bind(fs)),
-  map(extractVersionOf(package)),
-  reduce(buildDatabase, {}),
-  checkVersionResults,
-  checkInstalledVersion
+  filter(fs.existsSync.bind(fs))
 )(getPackageDirs());
+
+const valid = pipe(
+  map(package =>
+    pipe(
+      map(extractVersionOf(package)),
+      reduce(buildDatabase, {}),
+      checkVersionResults(package),
+      checkInstalledVersion(package)
+    )(packageTree)
+  ),
+  reduce(and, true)
+)(packages);
+
+if (valid) {
+  process.exit(0);
+} else {
+  process.exit(1);
+}
