03225e1d39997ecb1f718cb0035c5f6b2bb8fc7e
Dennis Sänger <dsaenger@rea.de>
2022-03-30 11:11:40 +0200

    feat: autopolyfill with core-js@3
    This should tell babel to add autopolyfills according to the specified
    version.

--- babelrc-ts.js
+++ babelrc-ts.js
@@ -1,36 +1,38 @@
 module.exports = {
   presets: [
-    '@babel/preset-react',
+    "@babel/preset-react",
     [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
+        useBuiltIns: "usage",
+        corejs: 3,
         targets: {
-          node: 6,
-          browsers: ['last 4 versions', 'safari >= 7']
-        }
-      }
+          node: 8,
+          browsers: ["last 4 versions"],
+        },
+      },
     ],
-    '@babel/preset-typescript',
-    '@rea-jet/babel-preset-rea'
+    "@babel/preset-typescript",
+    "@rea-jet/babel-preset-rea",
   ],
   env: {
     production: {
-      presets: ['minify'],
-      retainLines: false
-    }
+      presets: ["minify"],
+      retainLines: false,
+    },
   },
   plugins: [
-    '@babel/plugin-transform-modules-commonjs',
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-react-constant-elements',
+    "@babel/plugin-transform-modules-commonjs",
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-react-constant-elements",
     [
-      'module-resolver',
+      "module-resolver",
       {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      }
-    ]
-  ]
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    ],
+  ],
   // ignore: ['/node_modules/(?!__mocks__|__tests__)'],
   // retainLines: true
 };
--- babelrc.js
+++ babelrc.js
@@ -1,36 +1,38 @@
 module.exports = {
   presets: [
-    '@babel/preset-react',
+    "@babel/preset-react",
     [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
+        useBuiltIns: "usage",
+        corejs: 3,
         targets: {
-          node: 6,
-          browsers: ['last 4 versions', 'safari >= 7']
-        }
-      }
+          node: 8,
+          browsers: ["last 4 versions"],
+        },
+      },
     ],
-    '@babel/preset-flow',
-    '@rea-jet/babel-preset-rea'
+    "@babel/preset-flow",
+    "@rea-jet/babel-preset-rea",
   ],
   env: {
     production: {
-      presets: ['minify'],
-      retainLines: false
-    }
+      presets: ["minify"],
+      retainLines: false,
+    },
   },
   plugins: [
-    '@babel/plugin-transform-modules-commonjs',
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-react-constant-elements',
+    "@babel/plugin-transform-modules-commonjs",
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-react-constant-elements",
     [
-      'module-resolver',
+      "module-resolver",
       {
-        extensions: ['.js', '.jsx']
-      }
-    ]
-  ]
+        extensions: [".js", ".jsx"],
+      },
+    ],
+  ],
   // ignore: ['/node_modules/(?!__mocks__|__tests__)'],
   // retainLines: true
 };
